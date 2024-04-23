nabijaczleweli/hackerskeyboard

commit 859b0d490603aa013e15dc53adfc1d7bf4a80234

work around need to tap Back twice in some applications
Note that this only appears to affect apps that do not show
the software keyboard. These required a double-tap on the
Back button to actually signal the application.

Refer to https://issuetracker.google.com/issues/246132117#comment18

app/src/main/java/org/pocketworkstation/pckeyboard/LatinIME.java
@@ -727,6 +727,7 @@ public class LatinIME extends InputMethodService implements
         // setCandidatesViewShown(true);
         setCandidatesViewShownInternal(true, false);
         super.setCandidatesViewShown(true);
+        hideWindow();
     }
 
     @Override

commit d9a4041e463e5b9da9fd7b690d41fcc1c9435754

Don't hide keyboard on tap in ConnectBot (Fixes: 859b0d490603aa013e15dc53adfc1d7bf4a80234)

app/src/main/java/org/pocketworkstation/pckeyboard/LatinIME.java
@@ -727,7 +727,7 @@ public class LatinIME extends InputMethodService implements
         // setCandidatesViewShown(true);
         setCandidatesViewShownInternal(true, false);
         super.setCandidatesViewShown(true);
-        if (!restarting) {
+        if (!restarting && !isConnectbot()) {
             hideWindow();
         }
     }